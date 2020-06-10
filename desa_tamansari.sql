d21561353b9379ded2bf5c7e5bba1729fd7d45a0
Rizka <rizkaarachmawati998@gmail.com>
2020-06-10 13:25:28 +0700

    db

--- desa_tamansari.sql
+++ desa_tamansari.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2020 at 05:58 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
+-- Waktu pembuatan: 10 Jun 2020 pada 08.24
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,39 +25,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `profil`
---
-
-CREATE TABLE `profil` (
-  `id` varchar(30) NOT NULL,
-  `nama_lengkap` varchar(100) NOT NULL,
-  `alamat` varchar(200) NOT NULL,
-  `no_hp` varchar(14) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_bidang`
+-- Struktur dari tabel `tbl_bidang`
 --
 
 CREATE TABLE `tbl_bidang` (
   `id_bidang` int(11) NOT NULL,
-  `kode_rek` varchar(11) NOT NULL,
-  `Nama_rek` varchar(255) DEFAULT NULL
+  `id_tahun` int(11) DEFAULT NULL,
+  `kode_rek` varchar(11) DEFAULT NULL,
+  `nama_bidang` varchar(255) DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tbl_bidang`
---
-
-INSERT INTO `tbl_bidang` (`id_bidang`, `kode_rek`, `Nama_rek`) VALUES
-(21, '324', 'edfds');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_rkp`
+-- Struktur dari tabel `tbl_rkp`
 --
 
 CREATE TABLE `tbl_rkp` (
@@ -68,7 +49,7 @@ CREATE TABLE `tbl_rkp` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_sub_bidang`
+-- Struktur dari tabel `tbl_sub_bidang`
 --
 
 CREATE TABLE `tbl_sub_bidang` (
@@ -81,7 +62,7 @@ CREATE TABLE `tbl_sub_bidang` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_tahun`
+-- Struktur dari tabel `tbl_tahun`
 --
 
 CREATE TABLE `tbl_tahun` (
@@ -89,10 +70,22 @@ CREATE TABLE `tbl_tahun` (
   `tahun` int(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `tbl_tahun`
+--
+
+INSERT INTO `tbl_tahun` (`id_tahun`, `tahun`) VALUES
+(28, 2015),
+(29, 2016),
+(30, 2017),
+(31, 2018),
+(32, 2019),
+(33, 2020);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_user`
+-- Struktur dari tabel `tbl_user`
 --
 
 CREATE TABLE `tbl_user` (
@@ -103,27 +96,29 @@ CREATE TABLE `tbl_user` (
   `password` varchar(256) DEFAULT '',
   `role` int(2) NOT NULL,
   `is_active` int(1) NOT NULL,
-  `profil` int(30) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `agama` varchar(10) DEFAULT NULL,
+  `no_hp` int(11) DEFAULT NULL,
   `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_user`
+-- Dumping data untuk tabel `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`Id_user`, `username`, `email`, `image`, `password`, `role`, `is_active`, `profil`, `date_created`) VALUES
-(1, 'super admin', 'superAdmin@gmail.com', 'defaultP.jpg', '$2y$10$VXbZyE8av9sa4j5dY0vk4u5ex4H9j5y3LjB/o09wsgCnIa5lhm1rK', 1, 1, 0, '2020-05-10 13:14:21'),
-(2, 'admin', 'admin@gmail.com', 'defaultP.jpg', '$2y$10$9lHgS1CwCHKo1atb.efw7e/wcQEzrET5QfaH7e2oesMBQ11YRZSJS', 2, 1, 0, '2020-05-10 13:14:12');
+INSERT INTO `tbl_user` (`Id_user`, `username`, `email`, `image`, `password`, `role`, `is_active`, `alamat`, `agama`, `no_hp`, `date_created`) VALUES
+(12, 'admin', 'admin@gmail.com', 'defaultP.jpg', '$2y$10$TVhWpHQjCksiRbSgaqPpWOowUt4bbFlGYaX77S2SUmpjoVsU9tSiG', 2, 1, '', NULL, NULL, '0000-00-00 00:00:00'),
+(13, 'Rizka Rachmawati', 'superAdmin@gmail.com', 'defaultP.jpg', '$2y$10$8NZPwKVxU/NTC7NuX5GEfORxjNXwK6GQXUziOH0vQ3uZxbnOB7GBC', 1, 1, '', NULL, NULL, '2020-06-10 05:19:08');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_usulan`
+-- Struktur dari tabel `tbl_usulan`
 --
 
 CREATE TABLE `tbl_usulan` (
   `id_usulan` int(11) NOT NULL,
-  `id_tahun` int(11) DEFAULT NULL,
+  `id_bidang` int(11) DEFAULT NULL,
   `id_sub_bidang` int(11) DEFAULT NULL,
   `usulan` text DEFAULT NULL,
   `item` varchar(255) DEFAULT NULL,
@@ -132,144 +127,121 @@ CREATE TABLE `tbl_usulan` (
   `harga` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user_status`
---
-
-CREATE TABLE `user_status` (
-  `id` int(11) NOT NULL,
-  `status` varchar(128) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user_status`
---
-
-INSERT INTO `user_status` (`id`, `status`) VALUES
-(1, 'admin'),
-(2, 'user');
-
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `profil`
---
-ALTER TABLE `profil`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tbl_bidang`
+-- Indeks untuk tabel `tbl_bidang`
 --
 ALTER TABLE `tbl_bidang`
-  ADD PRIMARY KEY (`id_bidang`);
+  ADD PRIMARY KEY (`id_bidang`),
+  ADD KEY `fk_tahun` (`id_tahun`);
 
 --
--- Indexes for table `tbl_rkp`
+-- Indeks untuk tabel `tbl_rkp`
 --
 ALTER TABLE `tbl_rkp`
   ADD PRIMARY KEY (`id_rkp`),
   ADD KEY `fk_rkp_usulan` (`id_usulan`);
 
 --
--- Indexes for table `tbl_sub_bidang`
+-- Indeks untuk tabel `tbl_sub_bidang`
 --
 ALTER TABLE `tbl_sub_bidang`
   ADD PRIMARY KEY (`Id_sub_bidang`),
   ADD KEY `fk_sub_bidang` (`id_bidang`);
 
 --
--- Indexes for table `tbl_tahun`
+-- Indeks untuk tabel `tbl_tahun`
 --
 ALTER TABLE `tbl_tahun`
   ADD PRIMARY KEY (`id_tahun`),
   ADD KEY `fk_tahun_kegiUta` (`tahun`);
 
 --
--- Indexes for table `tbl_user`
+-- Indeks untuk tabel `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  ADD PRIMARY KEY (`Id_user`);
+  ADD PRIMARY KEY (`Id_user`),
+  ADD KEY `fk_profile` (`alamat`);
 
 --
--- Indexes for table `tbl_usulan`
+-- Indeks untuk tabel `tbl_usulan`
 --
 ALTER TABLE `tbl_usulan`
   ADD PRIMARY KEY (`id_usulan`),
-  ADD KEY `fk_usulan_Tahunn` (`id_tahun`),
-  ADD KEY `fk_usulan_sub` (`id_sub_bidang`);
+  ADD KEY `fk_usulan_sub` (`id_sub_bidang`),
+  ADD KEY `fk_bid` (`id_bidang`);
 
 --
--- Indexes for table `user_status`
---
-ALTER TABLE `user_status`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tbl_bidang`
+-- AUTO_INCREMENT untuk tabel `tbl_bidang`
 --
 ALTER TABLE `tbl_bidang`
-  MODIFY `id_bidang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_bidang` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `tbl_rkp`
+-- AUTO_INCREMENT untuk tabel `tbl_rkp`
 --
 ALTER TABLE `tbl_rkp`
   MODIFY `id_rkp` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `tbl_tahun`
+-- AUTO_INCREMENT untuk tabel `tbl_sub_bidang`
+--
+ALTER TABLE `tbl_sub_bidang`
+  MODIFY `Id_sub_bidang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_tahun`
 --
 ALTER TABLE `tbl_tahun`
-  MODIFY `id_tahun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_tahun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
--- AUTO_INCREMENT for table `tbl_user`
+-- AUTO_INCREMENT untuk tabel `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `Id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `Id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT for table `tbl_usulan`
+-- AUTO_INCREMENT untuk tabel `tbl_usulan`
 --
 ALTER TABLE `tbl_usulan`
   MODIFY `id_usulan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `user_status`
---
-ALTER TABLE `user_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `tbl_rkp`
+-- Ketidakleluasaan untuk tabel `tbl_bidang`
+--
+ALTER TABLE `tbl_bidang`
+  ADD CONSTRAINT `fk_tahun` FOREIGN KEY (`id_tahun`) REFERENCES `tbl_tahun` (`id_tahun`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_rkp`
 --
 ALTER TABLE `tbl_rkp`
-  ADD CONSTRAINT `fk_rkp_usulan` FOREIGN KEY (`id_usulan`) REFERENCES `tbl_usulan` (`id_usulan`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_usulan` FOREIGN KEY (`id_usulan`) REFERENCES `tbl_usulan` (`id_usulan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `tbl_sub_bidang`
+-- Ketidakleluasaan untuk tabel `tbl_sub_bidang`
 --
 ALTER TABLE `tbl_sub_bidang`
-  ADD CONSTRAINT `fk_sub_bidang` FOREIGN KEY (`id_bidang`) REFERENCES `tbl_bidang` (`id_bidang`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_bidang` FOREIGN KEY (`id_bidang`) REFERENCES `tbl_bidang` (`id_bidang`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `tbl_usulan`
+-- Ketidakleluasaan untuk tabel `tbl_usulan`
 --
 ALTER TABLE `tbl_usulan`
-  ADD CONSTRAINT `fk_usulan_sub` FOREIGN KEY (`id_sub_bidang`) REFERENCES `tbl_sub_bidang` (`Id_sub_bidang`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_usulan_tahun` FOREIGN KEY (`id_tahun`) REFERENCES `tbl_tahun` (`id_tahun`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_bid` FOREIGN KEY (`id_bidang`) REFERENCES `tbl_bidang` (`id_bidang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_subBi` FOREIGN KEY (`id_sub_bidang`) REFERENCES `tbl_sub_bidang` (`Id_sub_bidang`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
