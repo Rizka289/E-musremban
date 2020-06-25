1b69819a4b7acc9d2ed5bed3109ed4f6c2d9bd0e
Rizka <rizkaarachmawati998@gmail.com>
2020-06-25 13:45:45 +0700

    logout

--- desa_tamansari.sql
+++ desa_tamansari.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Jun 2020 pada 08.24
+-- Waktu pembuatan: 25 Jun 2020 pada 08.44
 -- Versi server: 10.4.8-MariaDB
 -- Versi PHP: 7.3.10
 
@@ -25,6 +25,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `desa`
+--
+
+CREATE TABLE `desa` (
+  `id_desa` int(11) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `password` varchar(150) DEFAULT NULL,
+  `alamat` varchar(250) DEFAULT NULL,
+  `agama` varchar(10) DEFAULT NULL,
+  `no_hp` varchar(12) DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `desa`
+--
+
+INSERT INTO `desa` (`id_desa`, `username`, `image`, `password`, `alamat`, `agama`, `no_hp`) VALUES
+(19, 'rizka', 'defaultP.jpg', '$2y$10$lW/ZIODAmOHpFNNChHFUxObekAFOzETJdsoE9ptJDdcGXjofyJ2fS', '2', '1', '5');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dusun`
+--
+
+CREATE TABLE `dusun` (
+  `id_dusun` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
+  `agama` varchar(10) NOT NULL,
+  `no_hp` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `dusun`
+--
+
+INSERT INTO `dusun` (`id_dusun`, `username`, `image`, `password`, `alamat`, `agama`, `no_hp`) VALUES
+(14, 'rahma', 'defaultP.jpg', '$2y$10$Lia9t.WKFfncdcwCmCKfK.6nHb2h6NwpyoKounwWDb5kFvOZXsZfW', '-', '-', '-');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tbl_bidang`
 --
 
@@ -34,6 +80,14 @@ CREATE TABLE `tbl_bidang` (
   `kode_rek` varchar(11) DEFAULT NULL,
   `nama_bidang` varchar(255) DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_bidang`
+--
+
+INSERT INTO `tbl_bidang` (`id_bidang`, `id_tahun`, `kode_rek`, `nama_bidang`) VALUES
+(1, 31, '23.3', 'ntahlah'),
+(10, 38, '2', 'fsdf');
 
 -- --------------------------------------------------------
 
@@ -59,6 +113,14 @@ CREATE TABLE `tbl_sub_bidang` (
   `nama_sub_bidang` varchar(255) DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `tbl_sub_bidang`
+--
+
+INSERT INTO `tbl_sub_bidang` (`Id_sub_bidang`, `id_bidang`, `Sub_rek`, `nama_sub_bidang`) VALUES
+(6, 1, '5667', 'iyyu'),
+(7, 1, '56', 'thb');
+
 -- --------------------------------------------------------
 
 --
@@ -75,40 +137,19 @@ CREATE TABLE `tbl_tahun` (
 --
 
 INSERT INTO `tbl_tahun` (`id_tahun`, `tahun`) VALUES
+(38, 342),
+(37, 587),
+(40, 2000),
 (28, 2015),
 (29, 2016),
 (30, 2017),
 (31, 2018),
 (32, 2019),
-(33, 2020);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tbl_user`
---
-
-CREATE TABLE `tbl_user` (
-  `Id_user` int(11) NOT NULL,
-  `username` varchar(128) DEFAULT '',
-  `email` varchar(128) NOT NULL,
-  `image` varchar(128) DEFAULT '',
-  `password` varchar(256) DEFAULT '',
-  `role` int(2) NOT NULL,
-  `is_active` int(1) NOT NULL,
-  `alamat` varchar(100) NOT NULL,
-  `agama` varchar(10) DEFAULT NULL,
-  `no_hp` int(11) DEFAULT NULL,
-  `date_created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `tbl_user`
---
-
-INSERT INTO `tbl_user` (`Id_user`, `username`, `email`, `image`, `password`, `role`, `is_active`, `alamat`, `agama`, `no_hp`, `date_created`) VALUES
-(12, 'admin', 'admin@gmail.com', 'defaultP.jpg', '$2y$10$TVhWpHQjCksiRbSgaqPpWOowUt4bbFlGYaX77S2SUmpjoVsU9tSiG', 2, 1, '', NULL, NULL, '0000-00-00 00:00:00'),
-(13, 'Rizka Rachmawati', 'superAdmin@gmail.com', 'defaultP.jpg', '$2y$10$8NZPwKVxU/NTC7NuX5GEfORxjNXwK6GQXUziOH0vQ3uZxbnOB7GBC', 1, 1, '', NULL, NULL, '2020-06-10 05:19:08');
+(33, 2021),
+(45, 2067),
+(43, 3242),
+(42, 6556),
+(39, 423432);
 
 -- --------------------------------------------------------
 
@@ -120,16 +161,35 @@ CREATE TABLE `tbl_usulan` (
   `id_usulan` int(11) NOT NULL,
   `id_bidang` int(11) DEFAULT NULL,
   `id_sub_bidang` int(11) DEFAULT NULL,
-  `usulan` text DEFAULT NULL,
-  `item` varchar(255) DEFAULT NULL,
-  `volume` int(11) DEFAULT NULL,
-  `satuan` varchar(20) DEFAULT NULL,
-  `harga` int(11) DEFAULT NULL
+  `usulan` varchar(100) DEFAULT '',
+  `anggaran` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_usulan`
+--
+
+INSERT INTO `tbl_usulan` (`id_usulan`, `id_bidang`, `id_sub_bidang`, `usulan`, `anggaran`) VALUES
+(8, 1, 6, 'Rizkaaaaa', 234),
+(9, 10, 7, 'aaaaa', 12),
+(10, 10, 7, '12', 1212),
+(11, 10, 6, 'dsfs', 1234);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `desa`
+--
+ALTER TABLE `desa`
+  ADD PRIMARY KEY (`id_desa`);
+
+--
+-- Indeks untuk tabel `dusun`
+--
+ALTER TABLE `dusun`
+  ADD PRIMARY KEY (`id_dusun`);
 
 --
 -- Indeks untuk tabel `tbl_bidang`
@@ -160,13 +220,6 @@ ALTER TABLE `tbl_tahun`
   ADD KEY `fk_tahun_kegiUta` (`tahun`);
 
 --
--- Indeks untuk tabel `tbl_user`
---
-ALTER TABLE `tbl_user`
-  ADD PRIMARY KEY (`Id_user`),
-  ADD KEY `fk_profile` (`alamat`);
-
---
 -- Indeks untuk tabel `tbl_usulan`
 --
 ALTER TABLE `tbl_usulan`
@@ -179,10 +232,22 @@ ALTER TABLE `tbl_usulan`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `desa`
+--
+ALTER TABLE `desa`
+  MODIFY `id_desa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `dusun`
+--
+ALTER TABLE `dusun`
+  MODIFY `id_dusun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- AUTO_INCREMENT untuk tabel `tbl_bidang`
 --
 ALTER TABLE `tbl_bidang`
-  MODIFY `id_bidang` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_bidang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `tbl_rkp`
@@ -194,25 +259,19 @@ ALTER TABLE `tbl_rkp`
 -- AUTO_INCREMENT untuk tabel `tbl_sub_bidang`
 --
 ALTER TABLE `tbl_sub_bidang`
-  MODIFY `Id_sub_bidang` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_sub_bidang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `tbl_tahun`
 --
 ALTER TABLE `tbl_tahun`
-  MODIFY `id_tahun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
-
---
--- AUTO_INCREMENT untuk tabel `tbl_user`
---
-ALTER TABLE `tbl_user`
-  MODIFY `Id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_tahun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT untuk tabel `tbl_usulan`
 --
 ALTER TABLE `tbl_usulan`
-  MODIFY `id_usulan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usulan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
